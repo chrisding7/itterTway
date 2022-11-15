60136582329d633823106a3186fa10866d986fa8
Chris Ding <chrisding7@berkeley.edu>
2022-11-15 11:57:32 -0800

    added login and signup components and routes, some styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Home () {
+    const [posts, setPosts] = useState([])
+
+    function handleSubmit(e) {
+        // post request for Post
+    }
+
     return (
-        <h1>Home</h1>
+        <div>
+            <h1>Home</h1>
+            <form className="post-form">
+                <textarea type ="text" placeholder="Compose Tweet..." maxlength={256} className="post-input"></textarea>
+                <button className="post-submit-btn" onSubmit={handleSubmit}>Post</button>
+            </form>
+            <div className="post-container">
+                <h2 className="post-display-name">Name</h2>
+                <h4 className="post-username">@username</h4>
+                <p className="post-content">Content</p>
+            </div>
+        </div>
     )
 }
 
