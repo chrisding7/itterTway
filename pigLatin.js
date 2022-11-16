7127f9ff2efe98f3e950291e390f6130077647ab
Chris Mann <chrispmann@gmail.com>
2022-11-16 08:07:02 -0800

    pig latin algorithm works except for punctuation

---
+++ pigLatin.js
@@ -0,0 +1,44 @@
+function pigLatinize(string){
+    const stringArray = string.split(" ")
+    
+    function isVowel(char)
+{
+    return char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U' || false;
+}
+    newStringArray = stringArray.map(element => {
+        if(isVowel(element[0])){
+            console.log(`first position vowel: ${element}`)
+            element = element.concat("way")
+            console.log(element)
+            return element
+
+
+        }
+        else if(isVowel(element[1]) && !isVowel(element[0])){
+            console.log(`second position vowel: ${element}`)
+            element = element.concat(element.at(0))
+            element = element.replace(element.at(0), "")
+            element = element.concat("ay")
+            console.log(element)
+            return element
+
+        }
+        else if(!isVowel(element[0]) && !isVowel(element[1])){
+            console.log(`neither first or second position vowel: ${element}`)
+            element = element.concat(element.at(0))
+            element = element.concat(element.at(1))
+            element = element.replace(element.at(0), "")
+            element = element.replace(element.at(0), "")
+            element = element.concat("ay")
+            console.log(element)
+            return element
+
+        }
+    });
+    newString = newStringArray.join(" ")
+    console.log(newString)
+    return newString
+
+}
+
+pigLatinize("I like to pet llamas with furry shawls")
