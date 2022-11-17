72e796690d9f0e1a04615e20ef4857694da30ddd
Justin Saborouh <rubikmanjustin@gmail.com>
2022-11-17 14:03:41 -0800

    Added posts index to main page

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :user, :text, :text_translated
-  has_many :comments
+  # has_many :comments
   has_one :user
 end
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import Post from './Post';
 
 function Home () {
 
@@ -6,7 +7,29 @@ function Home () {
         // post request for Post
     }
 
-    return (
+    const [allPosts, setAllPosts] = useState([])
+
+    useEffect(()=> {
+        fetch('/posts')
+        .then((res) => res.json())
+        .then(data => {
+            setAllPosts(data)
+            console.log(data)
+        })
+    }, []);
+
+    const RenderPosts = allPosts?.map((onePost) => {
+        return(
+            <Post
+            key={onePost.id}
+            user={onePost.user}
+            text={onePost.text}
+            textTran={onePost.text_translated}
+            />
+        )
+    })
+
+    return(
         <div>
             <h1>Home</h1>
             <form className="post-form">
@@ -14,12 +37,26 @@ function Home () {
                 <button className="post-submit-btn" onSubmit={handleSubmit}>Post</button>
             </form>
             <div className="post-container">
-                <h2 className="post-display-name">Name</h2>
-                <h4 className="post-username">@username</h4>
-                <p className="post-content">Content</p>
+                {RenderPosts}
             </div>
+            
         </div>
+
     )
+    // return (
+    //     <div>
+    //         <h1>Home</h1>
+    //         <form className="post-form">
+    //             <textarea type ="text" placeholder="Compose Tweet..." maxLength={256} className="post-input"></textarea>
+    //             <button className="post-submit-btn" onSubmit={handleSubmit}>Post</button>
+    //         </form>
+    //         <div className="post-container">
+    //             <h2 className="post-display-name">Name</h2>
+    //             <h4 className="post-username">@username</h4>
+    //             <p className="post-content">Content</p>
+    //         </div>
+    //     </div>
+    // )
 }
 
 export default Home
